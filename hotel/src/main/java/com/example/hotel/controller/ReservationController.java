54ade547e0843d41ab37843b8867866d7dc91fca
sm9940 <elation4927@gmail.com>
2024-04-13 22:18:29 +0900

    240413

--- hotel/src/main/java/com/example/hotel/controller/ReservationController.java
+++ hotel/src/main/java/com/example/hotel/controller/ReservationController.java
@@ -4,11 +4,15 @@ import com.example.hotel.dao.ReservationDao;
 import com.example.hotel.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ReservationController {
     @Autowired
     ReservationService reservationService;
 
-
+    @GetMapping("/reservation")
+    public String showReservationPage() {
+        return "reservation/reservation"; // 해당하는 HTML 파일의 이름 (예: reservation.html)
+    }
 }
