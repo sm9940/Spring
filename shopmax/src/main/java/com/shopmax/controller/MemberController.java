d84612b347471fbb94fec29ef032563e6ac8848b
sm9940 <elation4927@gmail.com>
2024-04-19 18:04:57 +0900

    240419

--- shopmax/src/main/java/com/shopmax/controller/MemberController.java
+++ shopmax/src/main/java/com/shopmax/controller/MemberController.java
@@ -1,2 +1,24 @@
-package com.shopmax.controller;public class MemberController {
+package com.shopmax.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    //문의하기
+    @GetMapping(value = "/members/qa")
+    public String qa(){
+        return "member/qa";
+    }
+    //로그인 화면
+    @GetMapping(value = "/members/login")
+    public String loginMember (){ return "member/memberLoginForm";}
+
+    @GetMapping(value = "/members/new")
+    public String memberForm(){
+        return "member/memberForm";
+    }
 }
