06a9a4c5cd9c1b117098067978540543c2849da8
sm9940 <elation4927@gmail.com>
2024-05-10 17:06:12 +0900

    240510

--- hospital/src/main/java/com/hospital/dto/ReserveHistDto.java
+++ hospital/src/main/java/com/hospital/dto/ReserveHistDto.java
@@ -1,6 +1,8 @@
 package com.hospital.dto;
 
 import com.hospital.constant.RStatus;
+import com.hospital.entity.Doctor;
+import com.hospital.entity.DoctorImg;
 import com.hospital.entity.Reservation;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,6 +22,18 @@ public class ReserveHistDto {
         this.rDate=reservation.getRDate();
         this.rTime=reservation.getRTime();
         this.rStatus=reservation.getRStatus();
+        Doctor doctor = reservation.getDoctor();
+
+        // 의사 객체가 null이 아닌 경우에만 이미지들을 가져와서 설정합니다.
+        if (doctor != null) {
+            List<DoctorImg> doctorImgs = doctor.getDoctorImgs();
+
+            // doctorImgs가 null이 아니고 비어있지 않은 경우에만 설정합니다.
+            if (doctorImgs != null && !doctorImgs.isEmpty()) {
+                // 여러 이미지 중 첫 번째 이미지를 설정합니다. 필요에 따라 다양한 방법으로 처리할 수 있습니다.
+                this.imgUrl = doctorImgs.get(0).getImgUrl();
+            }
+        }
     }
 
     private Long rId;
@@ -29,6 +43,7 @@ public class ReserveHistDto {
     private String rDate;
     private String rTime;
     private RStatus rStatus;
+    private String imgUrl;
 
     private List<ReservationDto> reservationDtoList =new ArrayList<>();
 
